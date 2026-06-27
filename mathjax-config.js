c4842e10d10b55a18d47f850efafb996cd545174
Adrián Gómez Sánchez <gomez.sanchez.adr@gmail.com>
2026-06-27 20:13:22 +0200

    Modularize LASLS presentation assets

---
+++ mathjax-config.js
@@ -0,0 +1,15 @@
+﻿/* ====================================================================
+   SECTION: MathJax configuration
+   ==================================================================== */
+window.MathJax = {
+    loader: { load: ['[tex]/html', '[tex]/boldsymbol'] },
+    startup: { typeset: false },
+    tex: {
+        packages: { '[+]': ['html', 'boldsymbol'] },
+        displayMath: [['\\[', '\\]']],
+        inlineMath: [['\\(', '\\)']]
+    },
+    chtml: {
+        matchFontHeight: false
+    }
+};
